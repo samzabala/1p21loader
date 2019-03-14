317561cec31e4b84a136a1b09098467d3d1235cc
Sam Z <samapitongzabala@gmail.com>
2019-03-13 22:03:06 -0700

    yeet

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
     </head>
     <body>
+
+    <!-- BEGIN loader -->
         <div id="loader">
             <div id="loader-wrapper">
             <div class="icon iconEnter">
@@ -17,69 +19,63 @@
                         <g class="car">
                             <g class="body">
                                 <!-- other dtails: behind body -->
-                                <polygon class="stroke-normal stroke-filled" points="330.4,99.2 372.9,74.9 380.1,95.3 379,113.3 375.9,116.8 377.6,129.2 382.8,134.5 381,145.9 376.4,146.1 366.6,151.7"/>
-
-                                <line class="stroke-bold" x1="317.5" y1="95" x2="321.4" y2="79.1"/>
+                                <polygon class="stroke-normal stroke-filled" points="357.4,99.2 399.9,74.9 407.1,95.3 406,113.3 402.9,116.8 404.6,129.2 409.8,134.5 408,145.9 403.4,146.1 393.6,151.7"/>
+                                <line class="stroke-bold" x1="344.5" y1="95" x2="348.4" y2="79.1"/>
                                 
                                 <!-- body outline-->
-                                <path class="stroke-normal stroke-filled" d="M124.6,166h9.9l-2.4, 7.3h143.1l-5-9.5H282h51.4l32.3-9.2c2.9-8.7,5-30.5-1.1-47.5l-89.3-20.7h-69.8 l-55.2,22.3l-81,10.8l-30.9,11.3l-3.2,9.7l-6,6.7l0.8,6.2h7.7l11.3,6.2h24.5L124.6,166z"/>
+                                <path class="stroke-normal stroke-filled" d="M151.6,166h9.9l-2.4,7.3h143.1l-5-9.5H309h51.4l32.3-9.2c2.9-8.7,5-30.5-1.1-47.5l-89.3-20.7h-69.8 l-55.2,22.3l-81,10.8l-30.9,11.3l-3.2,9.7l-6,6.7l0.8,6.2h7.7l11.3,6.2h24.5L151.6,166z"/>
                                 <!-- door -->
-                                <polyline class="stroke-normal" points="150.3,108.7 162.8,163.2 229.1,163.2 264.6,121.9 264.6,86.4"/>
+                                <polyline class="stroke-normal" points="177.3,108.7 189.8,163.2 256.1,163.2 291.6,121.9 291.6,86.4"/>
 
                                 <!-- back windshield -->
-                                <polyline class="stroke-normal"  points="275.4,86.4 275.4,114.5 346.6,114.5 343.1,102.1" />
-                                <polyline class="stroke-light" points="290.1,89.8 290.1,105.1 296.3,114.5"/>
-                                <polyline class="stroke-light" points="301.7,92.5 301.7,105.1 307.9,114.5"/>
-                                <polyline class="stroke-light" points="312.4,95 312.4,105.1 318.6,114.5"/>
+                                <polyline class="stroke-normal"  points="302.4,86.4 302.4,114.5 373.6,114.5 370.1,102.1"/>
+                                <polyline class="stroke-light" points="317.1,89.8 317.1,105.1 323.3,114.5"/>
+                                <polyline class="stroke-light" points="328.7,92.5 328.7,105.1 334.9,114.5"/>
+                                <polyline class="stroke-light" points="339.4,95 339.4,105.1 345.6,114.5"/>
 
                                 <!-- top part -->
-                                <line class="stroke-lighter" x1="258.3" y1="82.7" x2="271" y2="82.7"/>
-                                <line class="stroke-lighter" x1="265.6" y1="76.6" x2="265.6" y2="82.7"/>
-                                <line class="stroke-lighter" x1="256.2" y1="75.9" x2="272.4" y2="77.3"/>
+                                <line class="stroke-lighter" x1="285.3" y1="82.7" x2="298" y2="82.7"/>
+                                <line class="stroke-lighter" x1="292.6" y1="76.6" x2="292.6" y2="82.7"/>
+                                <line class="stroke-lighter" x1="283.2" y1="75.9" x2="299.4" y2="77.3"/>
 
                                 <!-- window -->
-                                <polygon class="stroke-normal" points="255.3,96.1 255.3,118.1 165.2,120.7 209.8,96.1"/>
-                                <polyline class="stroke-normal" points="190.5,120 212.8,106.3 236.7,106.3 243.6,118.4"/>
+                                <polygon class="stroke-normal" points="282.3,96.1 282.3,118.1 192.2,120.7 236.8,96.1"/>
+                                <polyline class="stroke-normal" points="217.5,120 239.8,106.3 263.7,106.3 270.6,118.4"/>
 
                                 <!-- body stroke -->
-                                <line class="stroke-lighter" x1="157.6" y1="140.5" x2="35.2" y2="140.5"/>
-                                <line class="stroke-lighter" x1="157.1" y1="138.5" x2="250.3" y2="138.5"/>
-                                <line class="stroke-lighter" x1="368.3" y1="140.5" x2="248.5" y2="140.5"/>
+                                <line class="stroke-lighter" x1="184.6" y1="140.5" x2="62.2" y2="140.5"/>
+                                <line class="stroke-lighter" x1="184.1" y1="138.5" x2="277.3" y2="138.5"/>
+                                <line class="stroke-lighter" x1="395.3" y1="140.5" x2="275.5" y2="140.5"/>
                                 
                                 <!-- other details: on body -->
-                                <line class="stroke-lighter" x1="247.6" y1="126.5" x2="247.6" y2="126.5"/>
-                                <line class="stroke-normal" x1="226.8" y1="144.5" x2="235.1" y2="144.5"/>
-                                <line class="stroke-normal" x1="57.5" y1="147.6" x2="64.1" y2="147.6"/>
-                                <line class="stroke-normal" x1="51.1" y1="146.8" x2="51.1" y2="150.7"/>
+                                <line class="stroke-lighter" x1="274.6" y1="126.5" x2="274.6" y2="126.5"/>
+                                <line class="stroke-normal" x1="253.8" y1="144.5" x2="262.1" y2="144.5"/>
+                                <line class="stroke-normal" x1="84.5" y1="147.6" x2="91.1" y2="147.6"/>
+                                <line class="stroke-normal" x1="78.1" y1="146.8" x2="78.1" y2="150.7"/>
 
                                 <g class="pole">
-                                    <line class="stroke-normal" x1="280.9" y1="87.7" x2="338.3" y2="30.3"/>
-                                    <path class="stroke-lighter" d="M338.3,30.3l4.9-4.9c0.9-0.9,2.4-1,3.4-0.2l1.3,1c1.5,1.3,3.8,1.1,5.1-0.3l6.1-6.5c2.1-2.2,1.9-5.7-0.4-7.7 l-1.7-1.4c-2.5-2.1-6.2-2-8.5,0.2l-0.2,0.2"/>
+                                    <line class="stroke-normal" x1="307.9" y1="87.7" x2="365.3" y2="30.3"/>
+                                    <path class="stroke-lighter" d="M365.3,30.3l4.9-4.9c0.9-0.9,2.4-1,3.4-0.2l1.3,1c1.5,1.3,3.8,1.1,5.1-0.3l6.1-6.5c2.1-2.2,1.9-5.7-0.4-7.7 l-1.7-1.4c-2.5-2.1-6.2-2-8.5,0.2l-0.2,0.2"/>
                                 </g>
                             </g>
 
 
                             <g class="wheels">
                                 <g class="wheel">
-                                    <circle class="stroke-normal stroke-filled" cx="99.3" cy="160.7" r="25.9"/>
-                                    <circle class="stroke-normal" cx="99.3" cy="160.5" r="16"/>
+                                    <circle class="stroke-normal stroke-filled" cx="126.3" cy="160.7" r="25.9"/>
+                                    <circle class="stroke-normal" cx="126.3" cy="160.5" r="16"/>
                                 </g>
                                 <g class="wheel">
-                                    <circle class="stroke-normal stroke-filled" cx="307.8" cy="160.7" r="25.9"/>
-                                    <circle class="stroke-normal" cx="307.8" cy="160.5" r="16"/>
+                                    <circle class="stroke-normal stroke-filled" cx="334.8" cy="160.7" r="25.9"/>
+                                    <circle class="stroke-normal" cx="334.8" cy="160.5" r="16"/>
                                 </g>
                             </g>
                         </g>
                         <g class="stripes">
-                            <!-- <line class="stripe" x1="461.4" y1="72.4" x2="378" y2="72.4"/> -->
-                            <!-- <line class="stripe" x1="570.8" y1="82.7" x2="404.3" y2="82.7"/> -->
-                            <!-- <line class="stripe" x1="503.9" y1="106.3" x2="386.6" y2="106.3"/> -->
-                            <!-- <line class="stripe" x1="509.5" y1="92.5" x2="412.6" y2="92.5"/> -->
-                            <!-- <line class="stripe" x1="523.6" y1="124.8" x2="393.7" y2="124.8"/> -->
-                            <line class="stripe" x1="560.6" y1="168.6" x2="343.7" y2="168.6"/>
-                            <line class="stripe" x1="506.3" y1="151.7" x2="416.2" y2="151.7"/>
-                            <line class="stripe" x1="423" y1="176.5" x2="332.9" y2="176.5"/>
-                            <line class="stripe" x1="445.3" y1="138.5" x2="398" y2="138.5"/>
+                        <line class="stripe" x1="587.6" y1="168.6" x2="370.7" y2="168.6"/>
+                        <line class="stripe" x1="533.3" y1="151.7" x2="443.2" y2="151.7"/>
+                        <line class="stripe" x1="450" y1="176.5" x2="359.9" y2="176.5"/>
+                        <line class="stripe" x1="472.3" y1="138.5" x2="425" y2="138.5"/>
                         </g>
 
 
@@ -91,16 +87,16 @@
                             <path class="glow accent" d="M218,44.4c24.2-19.2,45-21.3,52.1-2.2c9.5,26.1-10.7,82.2-45.3,125.2s-70.4,56.6-79.9,30.5"/> -->
 
 
-                            <path class="glow accent" d="M15.7,132.4L15.7,132.4L15.7,132.4c0-40,32.4-72.4,72.4-72.4h62.4"/>
-                            <path class="glow accent" d="M15.7,132.4L15.7,132.4L15.7,132.4c0-26.6,21.6-48.2,48.2-48.2h14.6"/>
-                            <path class="glow accent" d="M15.6,132.5L15.6,132.5L15.6,132.5c0-11.1,9-20.1,20.1-20.1h92.7"/>
-                            <path class="glow accent" d="M15.6,132.4L15.6,132.4L15.6,132.4c0,11.1,9,20.1,20.1,20.1h22.7"/>
-                            <path class="glow accent" d="M15.7,132.4L15.7,132.4L15.7,132.4c0,40,32.4,72.4,72.4,72.4h82.4"/>
-                            <path class="glow accent" d="M15.7,132.4L15.7,132.4L15.7,132.4c0,26.6,21.6,48.2,48.2,48.2h94.6"/>
-                            <line class="glow accent" x1="147.5" y1="167.4" x2="270" y2="167.4"/>
-                            <line class="glow accent" x1="148.9" y1="195.8" x2="305.8" y2="195.8"/>
-                            <line class="glow accent" x1="87.7" y1="68.4" x2="265.5" y2="68.4"/>
-                            <line class="glow accent" x1="157.3" y1="121" x2="352.9" y2="121"/>
+                            <path class="glow accent" d="M42.7,130.4L42.7,130.4L42.7,130.4c0-40,32.4-72.4,72.4-72.4h62.4"/>
+                            <path class="glow accent" d="M42.7,130.4L42.7,130.4L42.7,130.4c0-26.6,21.6-48.2,48.2-48.2h14.6"/>
+                            <path class="glow accent" d="M42.6,130.5L42.6,130.5L42.6,130.5c0-11.1,9-20.1,20.1-20.1h92.7"/>
+                            <path class="glow accent" d="M42.6,130.4L42.6,130.4L42.6,130.4c0,11.1,9,20.1,20.1,20.1h22.7"/>
+                            <path class="glow accent" d="M42.7,130.4L42.7,130.4L42.7,130.4c0,40,32.4,72.4,72.4,72.4h82.4"/>
+                            <path class="glow accent" d="M42.7,130.4L42.7,130.4L42.7,130.4c0,26.6,21.6,48.2,48.2,48.2h94.6"/>
+                            <line class="glow accent" x1="174.5" y1="165.4" x2="297" y2="165.4"/>
+                            <line class="glow accent" x1="175.9" y1="193.8" x2="332.8" y2="193.8"/>
+                            <line class="glow accent" x1="114.7" y1="66.4" x2="292.5" y2="66.4"/>
+                            <line class="glow accent" x1="184.3" y1="119" x2="379.9" y2="119"/>
                         </g>
                     </svg>
 
@@ -117,6 +113,16 @@
             </div>
             
         </div>
+        <!-- Script. Note that the added ready class must match the $docready-class variavle in scss/_vars.scss | move script to appropriate template part -->
+        <script>
+
+            document.addEventListener("DOMContentLoaded", function() {
+                setTimeout(function(){
+                    document.querySelector('body').classList.add('loaded');
+                },2000);
+            });
+        </script>
+    <!-- END LOADER -->
         
         <main>
             <h1>Hi this is the content</h1>
@@ -137,12 +143,5 @@
             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
 
         </main>
-        <script>
-            document.addEventListener("DOMContentLoaded", function() {
-                setTimeout(function(){
-                    document.querySelector('body').classList.add('loaded');
-                },2000);
-            });
-        </script>
     </body>
 </html>

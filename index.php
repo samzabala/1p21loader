a1748710fd5547d64f3358d4e6e4d0305148f7fe
Sam Zabala <samapitongzabala@gmail.com>
2019-03-25 09:33:36 -0700

    eee

--- index.php
+++ index.php
@@ -122,44 +122,45 @@
                     (function(global){
                         var DeloreanLoader = function(selector,numberSpan){
                             var _ = this; // avoid scoping issues
-                            numberSpan = numberSpan || '.number'; // defau
-                            _.loader  = document.querySelector(selector);
+                            numberSpan = numberSpan || '.number'; // container where number changes
+                            _.loader  = document.querySelector(selector); // loader dom
                             _.counter = 0;
                             _.counterContainer = _.loader.querySelector(numberSpan);
 
-                            _.randNum = function (min,max){
+                            _.randNum = function (min,max){ //make random bumbers
                                 return Math.round(Math.random()*(max-min))+min;
                             }
 
-                            _.progress = function(addToCount,fn,container){
-                                _.counter = (_.counter + addToCount < 100 ) ? _.counter + addToCount : 100;
-                                _.counterContainer.innerText = _.counter;
-                                fn && fn();
+                            _.progress = function(addToCount,callback,container){ //takes counter value and poots it in the counter container
+                                _.counter = (_.counter + addToCount < 100 ) ? (_.counter + addToCount) : 100; // must never be more than one hundred because wew
+                                _.counterContainer.innerText = _.counter; //poot
+                                callback && callback(); // run callback if it exists
                             };
 
                             _.cycle = function(){
-                                var addToCount = _.randNum(1,30),
-                                    toInterval = _.counter == 0 ? 1000 : _.randNum(10,500), //add after $loader-enter-duration 
-                                    fnToRun;
-                                if(_.counter < 100 && (_.counter + addToCount) < 100) {
+                                var addToCount = _.randNum(1,30), //number to add to counter
+                                    toInterval = _.counter == 0 ? 1000 : _.randNum(10,500), //1000 is the time mark after the set $loader-enter-duration. change if u change that css var too so the timing is nice
+                                    callback;
+                                if(_.counter < 100 && (_.counter + addToCount) < 100) {  // must never be more than one hundred because wew
                                     addToCount = addToCount,
-                                    fnToRun = _.cycle;
+                                    callback = _.cycle;
                                 }else{
-                                    addToCount = 100;
+                                    _.cycle(); //do it again and find a better number
                                 }
                                 
                                 setTimeout(function(){
-                                    _.progress( addToCount, fnToRun, _.counterContainer, );
+                                    _.progress( addToCount, callback, _.counterContainer ); //display progress
                                 },toInterval);
                             };
 
-                            _.killLoader = function(){
-                                setTimeout(function(){ 
-                                    if(_.counter == 100)  { 
+                            _.killLoader = function(){ //duh. called when shit ready. checks every millisecond after dom is ready that loader is ready too.
+                                var killerInterval = setTimeout(function(){ 
+                                    if(_.counter == 100)  { //
                                         console.log('Ready');
                                         document.querySelector('html').classList.add('loaded');
+                                        clearTimeout(killerInterval);
                                     }else{
-                                        _.killLoader()
+                                        _.killLoader() 
                                     };
                                 },100);
                             }
